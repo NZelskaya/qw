93dcdb144f06a32a37f1625c4c61535cb7080f2d
Antoni Piszczek <natali.zelskaya@gmail.com>
2019-01-16 11:20:42 +0100

    unused code removed

--- src/main/java/pl/b2b/net/driver/DriverSetup.java
+++ src/main/java/pl/b2b/net/driver/DriverSetup.java
@@ -1,42 +1,16 @@
 package pl.b2b.net.driver;
 
-import org.apache.log4j.Logger;
-import pl.b2b.net.utils.FileUtils;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
-import java.io.IOException;
 import java.util.concurrent.TimeUnit;
 
 import static pl.b2b.net.GlobalDefinitions.*;
-import static pl.b2b.net.utils.FileUtils.getResourceFilePath;
 
 public class DriverSetup {
     public static final String WEBDRIVER_RESOURCES = "c:\\tf\\tmp\\webdriver-resources\\";
     private static WebDriver driver;
-
-//    static {
-//        if (FileUtils.cleanExecutablesTmpDir()) {
-//            LOGGER.info("Temp resources folder cleaned up");
-//        } else {
-//            LOGGER.info("Temp resources folder was NOT removed");
-//        }
-//    }
-
-    private static void prepareSystemPropertyForBrowser(BrowserEnum browserEnum) throws IOException {
-        System.out.println("Preparing system for launching browser : " + browserEnum.toString());
-        String systemProperty = browserEnum.getSystemProperty();
-        String executableFile = browserEnum.getExecutableFile();
-
-        System.out.println("Checking system property : " + systemProperty);
-        if (System.getProperty(systemProperty) == null) {
-            System.out.println(String.format("System property %s is null. System property setup started", systemProperty));
-            String resourceFilePath = getResourceFilePath(executableFile);
-            System.setProperty(systemProperty, resourceFilePath);
-            System.out.println(systemProperty + " was set as = " + resourceFilePath);
-        }
-    }
 
     public static WebDriver createDriver(String browser) {
         try {
@@ -87,14 +61,8 @@ public class DriverSetup {
         return driver;
     }
 
-    public static void setDriver(String browser) throws Exception {
+    public static void setDriver(String browser) {
         System.out.println("Driver was set to = " + browser);
-
-        if (FileUtils.cleanExecutablesTmpDir()) {
-            System.out.println("Temp resources folder cleaned up");
-        } else {
-            System.out.println("Temp resources folder was NOT removed");
-        }
 
         createDriver(browser);
     }
